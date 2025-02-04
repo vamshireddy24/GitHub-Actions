6208b48508a6255b7a90f9f6c7c2cd826bbebc53
vamshireddy24 <88018641+vamshireddy24@users.noreply.github.com>
2025-02-04 11:11:27 +0000

    17-custom-actions-JS

--- .github/actions/js-dependency-update/index.js
+++ .github/actions/js-dependency-update/index.js
@@ -1,5 +1,48 @@
 const core = require('@actions/core')
+const exec = require('@actions/exec')
+
+const validateBranchName = ({ branchName }) => /^[a-zA-Z0-9_\-\.\/]+$/.test(branchName);
+const validateDirectoryName = ({ dirName }) => /^[a-zA-Z0-9_\-\/]+$/.test(dirName);
+
 async function run() {
+    const baseBranch = core.getInput('base-branch');
+    const targetBranch = core.getInput('target-branch');
+    const ghTocken = core.getInput('gh-tocken');
+    const workingDir = core.getInput('working-directory');
+    const debug = core.getBooleanInput('debug');
+    
+    core.setSecret(ghTocken);
+
+    if (validateBranchName({ branchName: baseBranch })) {
+        core.setFailed('Invalid base-branch name. Branch names should include only characters, numbers, hyphens, underscores, and forward slashes')
+        return;
+    }
+
+    if (validateBranchName({ branchName: targetBranch })) {
+        core.setFailed('Invalid target-branch name. Branch names should include only characters, numbers, hyphens, underscores, and forward slashes')
+        return;
+    }
+    if (!validateDirectoryName({ dirName: workingDir })) {
+        core.setFailed('Invalid working directory name. Directory names should include only characters, numbers, hyphens, underscores, and forward slashes')
+        return;
+    }
+
+    core.info('[js-dependency-update]: base branch is ${baseBranch}' );
+    core.info('[js-dependency-update]: target branch is ${targetBranch}' );
+    core.info('[js-dependency-update]: working directory is ${workingDir}' );
+    
+    await exec.exec('npm update',[],{
+        cwd: workingDir
+    });
+
+    const gitStatus = await exec.getExecOutput('git status -s package*.json', [], {
+        cwd: workingDir
+    });
+    if (gitStatus.stdout.length > 0){
+        core.info('[js-dependency-update] : There are updates available!')
+    } else {
+        core.info('[js-dependency-update] : No updates at this point in time.')
+    }
     /*
     1. Parse inputs:
        1.1 base-branch from which to check for updates
